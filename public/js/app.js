72c31c736ea8a2f24d1a00188bbd84a27d497790
cakdityo <cakdityo@gmail.com>
2016-12-11 09:26:51 +0700

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,26 @@
 var socket = io();
 
+// Listening for connection from server
 socket.on('connect', () => {
     alert('Browser connected to socket.io server!');
 });
+
+// Listening for message coming from other client in real - time;
+socket.on('message', (message) => {
+    console.log(message.user + ' said: ' + message.text);
+});
+
+function qs(query) {
+    return document.querySelector(query);
+}
+
+var randomUser = Math.floor((Math.random() * 100) + 1);
+
+function handleChatSubmit(e){
+    e.preventDefault();
+    var message = qs('textarea[name="textMessage"]');
+    socket.emit('message', {user: randomUser, text: message.value});
+    message.value = '';
+}
+
+qs('#chatForm').addEventListener('submit', handleChatSubmit);
--- server.js
+++ server.js
@@ -7,8 +7,21 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-io.on('connection', () => {
+// Listening for connection from client
+// Pass the individual connection instance to socket param
+io.on('connection', (socket) => {
     console.log('User connected via socket.io');
+
+    // Listening for message coming from client
+    // When received, emit the message to all connected clients
+    socket.on('message', (message) => {
+        io.emit('message', message);
+    });
+
+    // Emit a welcome message to a newly connected client
+    socket.emit('message', {
+        text: 'Welcome to the Real - Time World!'
+    });
 });
 
 http.listen(PORT, () => {
